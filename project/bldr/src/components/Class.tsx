f85efaf4a1c06369e3048072566fc288e6bb04b1
an-siuu-man <ansuman_sharma@protonmail.com>
2025-12-20 11:30:26 -0600

    added warning messages to calendar and class search for filled classes

--- project/bldr/src/components/Class.tsx
+++ project/bldr/src/components/Class.tsx
@@ -132,15 +132,10 @@ export default function Class(props: ClassProps) {
           </p>
           {classInfo.data[0].sections.map((section: ClassSection) => (
             <button
-              disabled={(section.seats_available ?? 0) <= 0}
+              // disabled={(section.seats_available ?? 0) <= 0}
               key={section.uuid}
               onClick={() => handleSectionClick(section, classInfo.data[0])}
-              className={
-                `w-full font-inter rounded-md mt-2 bg-[#181818] transition duration-100 px-2  text-left` +
-                ((section.seats_available ?? 0) > 0
-                  ? " cursor-pointer hover:bg-[#232323]"
-                  : " cursor-default opacity-60")
-              }
+              className={`w-full font-inter rounded-md mt-2 bg-[#181818] hover:bg-[#232323] transition duration-100 px-2 text-left cursor-pointer`}
             >
               <div className="flex flex-row w-full justify-between gap-3 items-start my-1">
                 <div className="flex flex-row gap-3 items-start">
